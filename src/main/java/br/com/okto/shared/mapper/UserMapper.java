871029cfed8a09831921a351c4b02dbd63d10503
caetanojpo <106156212+caetanojpo@users.noreply.github.com>
2025-04-26 10:53:47 -0300

    feat(adapter): user mapper

--- src/main/java/br/com/okto/shared/mapper/UserMapper.java
+++ src/main/java/br/com/okto/shared/mapper/UserMapper.java
@@ -3,16 +3,13 @@ package br.com.okto.shared.mapper;
 import br.com.okto.adapter.outboud.persistence.entity.UserEntity;
 import br.com.okto.domain.model.User;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "cdi")
 public interface UserMapper {
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
-    @Mapping(target = "password", source = "hashedPassword")
     User toDomain(UserEntity entity);
 
-    @Mapping(target = "hashedPassword", source = "password")
     UserEntity toEntity(User domain);
 }
