f629fb16c4e7e653e947ffbb9811b75ef6258484
caetanojpo <106156212+caetanojpo@users.noreply.github.com>
2025-04-26 10:53:37 -0300

    feat(adapter): update user entity with lombok

--- src/main/java/br/com/okto/adapter/outboud/persistence/entity/UserEntity.java
+++ src/main/java/br/com/okto/adapter/outboud/persistence/entity/UserEntity.java
@@ -7,12 +7,20 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 import java.util.UUID;
 
 @Entity
 @Table(name = "users")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
