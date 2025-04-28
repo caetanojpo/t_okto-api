e7a8e87f3992578229b80b4bfa0fdac4c4fd94a7
caetanojpo <106156212+caetanojpo@users.noreply.github.com>
2025-04-28 14:07:25 -0300

    feat(application): create user use case impl

--- src/main/java/br/com/okto/modules/user/application/usecase/CreateUserUseCaseImpl.java
+++ src/main/java/br/com/okto/modules/user/application/usecase/CreateUserUseCaseImpl.java
@@ -1,31 +1,45 @@
 package br.com.okto.modules.user.application.usecase;
 
+import br.com.okto.modules.user.adapter.out.persistence.entity.UserEntity;
+import br.com.okto.modules.user.application.dto.user.CreateUserRequest;
 import br.com.okto.modules.user.application.port.in.user.CreateUserUseCase;
 import br.com.okto.modules.user.application.port.in.user.FindUserUseCase;
 import br.com.okto.modules.user.application.port.out.UserRepository;
-import br.com.okto.modules.user.domain.model.User;
+import br.com.okto.modules.user.domain.enums.UserRole;
 import br.com.okto.shared.exception.ConflictException;
+import br.com.okto.shared.mapper.UserMapper;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import lombok.RequiredArgsConstructor;
 
+import java.util.Date;
+import java.util.UUID;
 
+@ApplicationScoped
 public class CreateUserUseCaseImpl implements CreateUserUseCase {
 
     private final UserRepository repository;
-    private final FindUserUseCase find;
+    private static final UserMapper mapper = UserMapper.INSTANCE;
 
-    public CreateUserUseCaseImpl(UserRepository repository, FindUserUseCase find){
+    @Inject
+    public CreateUserUseCaseImpl(UserRepository repository) {
         this.repository = repository;
-        this.find = find;
     }
 
     @Override
-    public void execute(User userData) {
-        User foundUser = find.executeByEmail(userData.getEmail());
+    public UUID execute(CreateUserRequest userData) {
 
-        if(foundUser != null){
-            throw new ConflictException("User", userData.getEmail());
-        }
+        UserEntity userToBeCreated = UserEntity.builder()
+                .name(userData.name())
+                .email(userData.email())
+                .hashedPassword(userData.password())
+                .isActive(true)
+                .role(UserRole.USER)
+                .createdAt(new Date())
+                .updatedAt(new Date())
+                .build();
 
-        userData.setActive(true);
-        repository.saveUser(userData);
+        repository.saveUser(userToBeCreated);
+        return userToBeCreated.getId();
     }
 }
