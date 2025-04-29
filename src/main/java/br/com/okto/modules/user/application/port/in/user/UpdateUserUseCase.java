515f9b2f7766c91ddf4baf26bb62ccaabd3e56ae
caetanojpo <106156212+caetanojpo@users.noreply.github.com>
2025-04-29 16:35:51 -0300

    feat(application): update user use case

--- src/main/java/br/com/okto/modules/user/application/port/in/user/UpdateUserUseCase.java
+++ src/main/java/br/com/okto/modules/user/application/port/in/user/UpdateUserUseCase.java
@@ -1,7 +1,15 @@
 package br.com.okto.modules.user.application.port.in.user;
 
+import br.com.okto.modules.user.application.dto.user.UpdateUserBasicInfoRequest;
+import br.com.okto.modules.user.application.dto.user.UpdateUserPasswordRequest;
+import br.com.okto.modules.user.application.dto.user.UpdateUserRoleRequest;
+import br.com.okto.modules.user.domain.enums.UserRole;
 import br.com.okto.modules.user.domain.model.User;
 
+import java.util.UUID;
+
 public interface UpdateUserUseCase {
-    void execute(User userData);
+    void executeChangePassword(UpdateUserPasswordRequest userData, UUID userId);
+    void executeChangeBasicInfo(UpdateUserBasicInfoRequest userData, UUID userId);
+    void executeChangeUserRole(UpdateUserRoleRequest userData, UUID userId);
 }
---
+++ src/main/java/br/com/okto/modules/user/application/usecase/UpdateUserUseCaseImpl.java
@@ -0,0 +1,75 @@
+package br.com.okto.modules.user.application.usecase;
+
+import br.com.okto.modules.user.application.dto.user.UpdateUserBasicInfoRequest;
+import br.com.okto.modules.user.application.dto.user.UpdateUserPasswordRequest;
+import br.com.okto.modules.user.application.dto.user.UpdateUserRoleRequest;
+import br.com.okto.modules.user.application.port.in.user.UpdateUserUseCase;
+import br.com.okto.modules.user.application.port.out.UserRepository;
+import br.com.okto.modules.user.domain.enums.UserRole;
+import br.com.okto.modules.user.domain.model.User;
+import br.com.okto.shared.exception.EntityNotFoundException;
+import br.com.okto.shared.mapper.UserMapper;
+import jakarta.enterprise.context.ApplicationScoped;
+
+import java.util.Date;
+import java.util.UUID;
+
+@ApplicationScoped
+public class UpdateUserUseCaseImpl implements UpdateUserUseCase {
+    private final UserRepository repository;
+
+    public UpdateUserUseCaseImpl(UserRepository repository){
+        this.repository = repository;
+    }
+
+    @Override
+    public void executeChangePassword(UpdateUserPasswordRequest userData, UUID userId) {
+        var existingUser = repository.findUserById(userId);
+
+        if(existingUser == null || !existingUser.isActive()){
+            throw new EntityNotFoundException("USER", userId.toString(), "id");
+        }
+
+        String newUserPassword = userData.password();
+
+        existingUser.setHashedPassword(newUserPassword);
+        existingUser.setUpdatedAt(new Date());
+
+        repository.updateUser(existingUser);
+    }
+
+    @Override
+    public void executeChangeBasicInfo(UpdateUserBasicInfoRequest userData, UUID userId) {
+        var existingUser = repository.findUserById(userId);
+
+        if(existingUser == null || !existingUser.isActive()){
+            throw new EntityNotFoundException("USER", userId.toString(), "id");
+        }
+
+        existingUser.setName(validateForEmptyValue(userData.name(), existingUser.getName()));
+        existingUser.setEmail(validateForEmptyValue(userData.email(), existingUser.getEmail()));
+        existingUser.setUpdatedAt(new Date());
+
+        repository.updateUser(existingUser);
+    }
+
+    @Override
+    public void executeChangeUserRole(UpdateUserRoleRequest userData, UUID userId) {
+        var existingUser = repository.findUserById(userId);
+
+        if(existingUser == null || !existingUser.isActive()){
+            throw new EntityNotFoundException("USER", userId.toString(), "id");
+        }
+
+        UserRole newUserRole = userData.role();
+
+        existingUser.setRole(newUserRole);
+        existingUser.setUpdatedAt(new Date());
+
+        repository.updateUser(existingUser);
+    }
+
+    private String validateForEmptyValue(String newValue, String currentValue) {
+        return newValue != null && !newValue.trim().isEmpty() ? newValue : currentValue;
+    }
+}
